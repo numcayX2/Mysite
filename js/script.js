cf6d4ca9518ad006c66c480d2891887c0ced9c97
numcayX2 <54502307+numcayX2@users.noreply.github.com>
2023-09-17 01:09:49 +0700

    1.0.1

--- js/script.js
+++ js/script.js
@@ -1,6 +1,6 @@
 const scrollElement = document.querySelector('.to-top');
 window.addEventListener('scroll', () => {
-    if (window.scrollY === 10) {
+    if (window.scrollY === 0) {
       scrollElement.classList.add('scrolled');
     } else {
       scrollElement.classList.remove('scrolled');
